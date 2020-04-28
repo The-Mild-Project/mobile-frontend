99e663f51ffe4c3b7e8754e9db2c467f68490bed
Nick Kebbas <nkebbas@usfca.edu>
2020-04-28 15:56:11 -0700

    connecting mobile to backend oauth still wip

--- screens/LoginPage.js
+++ screens/LoginPage.js
@@ -13,38 +13,67 @@ function LoginPage({ navigation }) {
 async function asyncGetDataHelper(key) {
     return await AsyncStorage.getItem(key);
 }
+
 async function googleLogin(navigation) {
     // wait for access token from Expo's Google API
-    const { type, accessToken, user } = await Google.logInAsync({
+    const result = await Google.logInAsync({
         iosClientId: CONFIG.IOSCLIENTID,
-        androidClientId: CONFIG.ANDROIDCLIENTID,
-        //iosStandaloneAppClientId: `<YOUR_IOS_CLIENT_ID>`,
-        //androidStandaloneAppClientId: `<YOUR_ANDROID_CLIENT_ID>`,
+        androidClientId: CONFIG.ANDROIDCLIENTID
     });
 
-    if (type === 'success') {
+    if (result.type === 'success') {
+        let userInfoResponse = await fetch('https://www.googleapis.com/userinfo/v2/me', {
+            headers: { Authorization: `Bearer ${result.accessToken}`},
+        });
         /* `accessToken` is now valid and can be used to get data from the Google API with HTTP requests */
         // store token
-        console.log(typeof(accessToken))
-        AsyncStorage.setItem('tokenId', accessToken, () => {
-            AsyncStorage.mergeItem('tokenId', accessToken, () => {
+        console.log(typeof(result.accessToken))
+        AsyncStorage.setItem('tokenId', result.accessToken, () => {
+            AsyncStorage.mergeItem('tokenId', result.accessToken, () => {
                 AsyncStorage.getItem('tokenId', (err, result) => {
                     console.log(result);
                 });
             });
         });
 
-        AsyncStorage.setItem('name', user.name, () => {
-            AsyncStorage.mergeItem('name', user.name, () => {
+        AsyncStorage.setItem('name', result.user.name, () => {
+            AsyncStorage.mergeItem('name', result.user.name, () => {
                 AsyncStorage.getItem('name', (err, result) => {
                     console.log(result);
                 });
             });
         });
-        console.log(user);
+        console.log(result.user);
+        console.log(typeof(result.user));
         //console.log(accessToken);
-        navigation.navigate('LoggedInScreen', {"name": user.name})
+        // send a request to the database to check if user exists
+        console.log(result.user.id);
+        console.log(typeof(result.user.id));
+        await passTokenToBackend(result);
+        navigation.navigate('LoggedInScreen', {"name": result.user.name})
         //return accessToken;
+    }
+}
+
+async function passTokenToBackend(result) {
+    try {
+        let response = await fetch('https://foodapp-user-service.herokuapp.com/test/user/login', {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'googleId': result.accessToken
+            }
+        });
+        console.log("repsonse");
+        console.log(JSON.stringify(response));
+        if (response.status === 415) {
+            return;
+        }
+        let responseJson = await response.json();
+        return responseJson;
+    } catch (error) {
+        console.error(error);
     }
 }
 
