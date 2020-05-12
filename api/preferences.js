1af76213d61f607bc7c056f3b049860e55c0570b
Nick Kebbas <nkebbas@usfca.edu>
2020-05-11 23:58:39 -0700

    trigger build process

--- api/preferences.js
+++ api/preferences.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8080/test/preferences',
+    baseURL: 'http://localhost:8080/test/preferences' ,
     headers : {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': true
